2170c4ea039f8fac2a28ffa2584b04ed8c65b19e
detached64 <169419571+detached64@users.noreply.github.com>
2025-02-10 12:11:39 +0800

    Initial

---
+++ SiglusKeyFinder/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SiglusKeyFinder")]
+[assembly: AssemblyDescription("Catch the SiglusEngine Encryption Key with this analyzer")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("VNX+")]
+[assembly: AssemblyProduct("SiglusKeyFinder")]
+[assembly: AssemblyCopyright("Copyright © VNX+ 2016-2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("fb2b63fe-b83c-4a27-86f0-aa67f16956fd")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.29.0.0")]
+[assembly: AssemblyFileVersion("2.32.5.2017")]
